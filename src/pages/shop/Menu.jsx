a05c3afc15c422a9614743442f4ebb54301d8365
Janindu Dissanayaka <janidu.22@cse.mrt.ac.lk>
2025-07-18 17:15:29 +0530

    Enhance Menu component with filtering and sorting options; improve UI for category selection

--- src/pages/shop/Menu.jsx
+++ src/pages/shop/Menu.jsx
@@ -75,12 +75,30 @@ const Menu = () => {
                 </div>
         </div>
         <div className='section-container'>
-          //filtering and sorting
+          {/* filtering and sorting */}
            <div>
+             <div className='flex justify-between items-center mb-6'>
+                <div className='flex gap-4'>
+                  <button onClick={() => showAll()} className={`btn ${selectesCategory === 'All' ? 'bg-green-500 text-white' : 'bg-gray-200 text-black'} px-4 py-2 rounded`}>All</button>
+                  <button onClick={() => filterItems('salad')} className={`btn ${selectesCategory === 'salad' ? 'bg-green-500 text-white' : 'bg-gray-200 text-black'} px-4 py-2 rounded`}>Salad</button>
+                  <button onClick={() => filterItems('pizza')} className={`btn ${selectesCategory === 'pizza' ? 'bg-green-500 text-white' : 'bg-gray-200 text-black'} px-4 py-2 rounded`}>Pizza</button>
+                  <button onClick={() => filterItems('soup')} className={`btn ${selectesCategory === 'soup' ? 'bg-green-500 text-white' : 'bg-gray-200 text-black'} px-4 py-2 rounded`}>Soups</button>
+                  <button onClick={() => filterItems('dessert')} className={`btn ${selectesCategory === 'dessert' ? 'bg-green-500 text-white' : 'bg-gray-200 text-black'} px-4 py-2 rounded`}>Desserts</button>
+                  <button onClick={() => filterItems('drinks')} className={`btn ${selectesCategory === 'drinks' ? 'bg-green-500 text-white' : 'bg-gray-200 text-black'} px-4 py-2 rounded`}>Drinks</button>
 
+                </div>
+                <div>
+                  <select value={sortOption} onChange={(e) => handleSortChange(e.target.value)} className='select select-bordered border-black w-full max-w-xs bg-white text-black'>
+                    <option value="default">Sort By</option>
+                    <option value="A-Z">A-Z</option>
+                    <option value="Z-A">Z-A</option>
+                    <option value="low-to-high">Price: Low to High</option>
+                  </select>
+                </div>
+              </div>
            </div>
-           //products card
-           <div>
+           {/* products card */}
+           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {
               filteredItems.map((item) => (
                 <Cards key={item.id} item={item}/>
