3be9fe3b566d12a02b76e7b0abae77e194f95775
Janindu Dissanayaka <janidu.22@cse.mrt.ac.lk>
2025-07-19 20:24:13 +0530

    Add react-hook-form integration to Modal component for improved form handling

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 import { FaFacebook, FaGoogle, FaInstagram } from 'react-icons/fa'; // commonly used for Google login
 
 const Modal = () => {
+    const { register, handleSubmit,  formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
   return (
     <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
       <div className="modal-box bg-white rounded-xl  py-4 px-8">
         <h3 className="font-bold text-2xl text-green-600 mb-2">Welcome Back</h3>
         <p className="mb-6 text-gray-500">Sign in to continue. Press ESC or click below to close.</p>
-        <form className="space-y-5 bg-white px-4 rounded-lg ">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5 bg-white px-4 rounded-lg ">
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1 bg-amber-50">Email</label>
-            <input type="email" className="input input-bordered w-full bg-amber-50 border-black" placeholder="you@email.com" />
+            <input type="email" className="input input-bordered w-full bg-amber-50 border-black" required {...register("email")} />
           </div>
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1 bg-amber-50">Password</label>
-            <input type="password" className="input input-bordered w-full bg-amber-50 border-black" placeholder="Password" />
+            <input type="password" className="input input-bordered w-full bg-amber-50 border-black" placeholder="Password" required {...register("password")} />
           </div>
           <div className="flex justify-between items-center">
             <a className="text-green-600 hover:underline text-sm" href="#">Forgot password?</a>
