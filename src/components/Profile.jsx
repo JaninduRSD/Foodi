eb6fffd8007d28388919e2131804c7c81d69f7d8
Janindu Dissanayaka <janidu.22@cse.mrt.ac.lk>
2025-07-23 02:07:35 +0530

    feat: update Profile component to display user photo in drawer toggle

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,7 +6,17 @@ const Profile = ({user}) => {
   <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
   <div className="drawer-content">
     {/* Page content here */}
-    <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary">Open drawer</label>
+    <label htmlFor="my-drawer-4" className="drawer-button btn btn-ghost btn-circle ">
+        <div className="w-10 rounded-full">
+          {
+            user.photoURL? <img className='rounded-full' 
+            alt="Tailwind CSS Navbar component"
+            src= {user.photoURL} />: <img className='rounded-full'
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+          }
+        </div>
+    </label>
   </div>
   <div className="drawer-side">
     <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
