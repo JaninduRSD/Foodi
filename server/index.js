8351bedac802e67f885a44a8b51999240a7210cd
Janindu Dissanayaka <janidu.22@cse.mrt.ac.lk>
2025-07-26 10:03:11 +0530

    comit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,53 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 6001;
+require('dotenv').config()
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+//mongodb config
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@demo-foodi-cluster.h4qqk8i.mongodb.net/?retryWrites=true&w=majority&appName=demo-foodi-cluster`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    //database & collection
+    const menuCollections = client.db("demo-foodi-client").collection("menu");
+    const cartCollections = client.db("demo-foodi-client").collection("cartItems");
+
+
+//all menu items operations
+    app.get('/menu', async(req,res)=>{
+        const result = await menuCollections.find().toArray();
+        res.send(result);
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
