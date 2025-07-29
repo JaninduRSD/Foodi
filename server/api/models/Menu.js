d4baa2481e2ad1472bd29b9dc95d35261f38cbb6
Janindu Dissanayaka <janidu.22@cse.mrt.ac.lk>
2025-07-29 13:27:41 +0530

    commit

---
+++ server/api/models/Menu.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+//create schema object for menu items
+const menuSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 3
+    },
+    recipe:String,
+    image: String,
+    category : String,
+    Price: Number
+})
+
+//create model
+const Menu = mongoose.model('Menu', menuSchema)
