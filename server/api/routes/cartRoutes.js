35f8dfae90e93d3718e79648128884bff4ebb81c
Janindu Dissanayaka <janidu.22@cse.mrt.ac.lk>
2025-07-30 09:30:11 +0530

    cart routes

--- server/api/routes/cartRoutes.js
+++ server/api/routes/cartRoutes.js
@@ -7,5 +7,10 @@ const cartController = require('../controllers/cartControllers')
 //get all menu items
 router.get('/', cartController.getCartByEmail)
 router.post('/', cartController.addToCart)
+router.delete('/:id',cartController.deleteCart)
+router.put('/:id',cartController.updateCart)
+router.get('/:id',cartController.getSingleCart)
+
+
 
 module.exports = router;
