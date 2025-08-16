c096a2ed10c53fdddcc76b936b006901f9abdf44
Janindu Dissanayaka <janidu.22@cse.mrt.ac.lk>
2025-08-16 23:56:59 +0530

    commit

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { FaEdit, FaPlusCircle, FaShoppingBag, FaUser } from 'react-icons/fa'
 import { MdDashboard } from 'react-icons/md'
 import { Link } from 'react-router'
-import logo from '/logo.png'
+import logo from '/logo (1).png'
 
 const DashboardLayout = () => {
   return (
-    <div className="drawer lg:drawer-open">
+    <div className="drawer lg:drawer-open bg-amber-50">
     <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
     <div className="drawer-content flex flex-col items-center justify-center">
         {/* Page content here */}
@@ -14,9 +14,9 @@ const DashboardLayout = () => {
         Open drawer
         </label>
     </div>
-    <div className="drawer-side">
-        <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+    <div className="drawer-side bg-amber-50">
+        <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay bg-amber-50"></label>
+        <ul className="menu bg-gray-50 text-black min-h-full w-80 p-4 ">
         {/* Sidebar content here */}
         <li><Link to='/dashboard'><img src={logo} alt='' className='w-20'/></Link></li>
         <li><Link to='/dashboard'><MdDashboard/>Dashboard</Link></li>
